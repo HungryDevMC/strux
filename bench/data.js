ab2118d1c13e5fe8ef4b8466fbe0c901d09d306c
github-action-benchmark <github@users.noreply.github.com>
2026-07-03 13:59:00 +0000

    add strux JMH benchmarks (jmh) benchmark result for ab3c3444896b34265e4706da18215c2d88b0ed49

---
+++ bench/data.js
@@ -0,0 +1,104 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1783087140561,
+  "repoUrl": "https://github.com/HungryDevMC/strux",
+  "entries": {
+    "strux JMH benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "algorithm1401@gmail.com",
+            "name": "HungryDevMC",
+            "username": "HungryDevMC"
+          },
+          "committer": {
+            "email": "algorithm1401@gmail.com",
+            "name": "HungryDevMC",
+            "username": "HungryDevMC"
+          },
+          "distinct": true,
+          "id": "ab3c3444896b34265e4706da18215c2d88b0ed49",
+          "message": "docs: add release, platform, and benchmark badges",
+          "timestamp": "2026-07-03T15:57:44+02:00",
+          "tree_id": "7bb1aa40c6518c5840d1a4e5050d2f2b4a273f36",
+          "url": "https://github.com/HungryDevMC/strux/commit/ab3c3444896b34265e4706da18215c2d88b0ed49"
+        },
+        "date": 1783087140204,
+        "tool": "jmh",
+        "benches": [
+          {
+            "name": "dev.gesp.structural.bench.CascadeBenchmark.bridgeTrimBack",
+            "value": 170.87219426739458,
+            "unit": "us/op",
+            "extra": "iterations: 3\nforks: 1\nthreads: 1"
+          },
+          {
+            "name": "dev.gesp.structural.bench.CascadeBenchmark.columnFullCollapse",
+            "value": 70.65577709978231,
+            "unit": "us/op",
+            "extra": "iterations: 3\nforks: 1\nthreads: 1"
+          },
+          {
+            "name": "dev.gesp.structural.bench.CascadeBenchmark.siegeTowerLarge",
+            "value": 557685.9508333333,
+            "unit": "us/op",
+            "extra": "iterations: 3\nforks: 1\nthreads: 1"
+          },
+          {
+            "name": "dev.gesp.structural.bench.CascadeBenchmark.siegeTowerSmall",
+            "value": 45375.49038210678,
+            "unit": "us/op",
+            "extra": "iterations: 3\nforks: 1\nthreads: 1"
+          },
+          {
+            "name": "dev.gesp.structural.bench.ScopedSolveBenchmark.solveOneComponent ( {\"structures\":\"1\"} )",
+            "value": 5.221697136627257,
+            "unit": "us/op",
+            "extra": "iterations: 3\nforks: 1\nthreads: 1"
+          },
+          {
+            "name": "dev.gesp.structural.bench.ScopedSolveBenchmark.solveOneComponent ( {\"structures\":\"50\"} )",
+            "value": 5.016381433575481,
+            "unit": "us/op",
+            "extra": "iterations: 3\nforks: 1\nthreads: 1"
+          },
+          {
+            "name": "dev.gesp.structural.bench.ScopedSolveBenchmark.solveOneComponent ( {\"structures\":\"200\"} )",
+            "value": 5.21643780323837,
+            "unit": "us/op",
+            "extra": "iterations: 3\nforks: 1\nthreads: 1"
+          },
+          {
+            "name": "dev.gesp.structural.bench.ScopedSolveBenchmark.solveWholeWorld ( {\"structures\":\"1\"} )",
+            "value": 5.22546313769928,
+            "unit": "us/op",
+            "extra": "iterations: 3\nforks: 1\nthreads: 1"
+          },
+          {
+            "name": "dev.gesp.structural.bench.ScopedSolveBenchmark.solveWholeWorld ( {\"structures\":\"50\"} )",
+            "value": 542.8995048516408,
+            "unit": "us/op",
+            "extra": "iterations: 3\nforks: 1\nthreads: 1"
+          },
+          {
+            "name": "dev.gesp.structural.bench.ScopedSolveBenchmark.solveWholeWorld ( {\"structures\":\"200\"} )",
+            "value": 3411.8216639691145,
+            "unit": "us/op",
+            "extra": "iterations: 3\nforks: 1\nthreads: 1"
+          },
+          {
+            "name": "dev.gesp.structural.bench.SolverBenchmark.solveAll ( {\"height\":\"4\"} )",
+            "value": 47.846233832860094,
+            "unit": "us/op",
+            "extra": "iterations: 3\nforks: 1\nthreads: 1"
+          },
+          {
+            "name": "dev.gesp.structural.bench.SolverBenchmark.solveAll ( {\"height\":\"10\"} )",
+            "value": 126.81732110497954,
+            "unit": "us/op",
+            "extra": "iterations: 3\nforks: 1\nthreads: 1"
+          }
+        ]
+      }
+    ]
+  }
+}
